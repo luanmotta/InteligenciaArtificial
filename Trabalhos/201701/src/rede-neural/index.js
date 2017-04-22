04bdb1153d270d35bfc66701ee30f9fd47c25c1f
Ezequiel <ezequielmross@gmail.com>
2017-04-22 12:49:28 -0300

    added csvtojson

--- Trabalhos/201701/src/rede-neural/index.js
+++ Trabalhos/201701/src/rede-neural/index.js
@@ -1,6 +1,26 @@
+const
+    csv = require('csvtojson');
+
+let
+    list = [];
+
 class Main {
     constructor() {
         console.log("node started");
+
+        csv({
+            'delimiter': ';'
+        })
+        .fromFile(__dirname + '/winequality-red.csv')
+        .on('json', (jsonObj) => {
+            //console.log(jsonObj[0]);
+            list.push(jsonObj);
+        })
+        .on('done', (error, data) => {
+            if (error)
+                console.log(error)
+            console.log(list[0]);
+        })
     }
 }
 

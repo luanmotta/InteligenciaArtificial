55b6e3975361f03e46363a83763bab42170a5f0f
Ezequiel <ezequielmross@gmail.com>
2017-04-23 16:19:34 -0300

    improve treinamento

--- Trabalhos/201701/src/rede-neural/MLP.js
+++ Trabalhos/201701/src/rede-neural/MLP.js
@@ -1,14 +1,23 @@
 const Neuronio = require('./Neuronio.js');
 
+function getNeuronios(length, layerIndex, config) {
+  const neuronios = [];
+  
+  for (var i = 0; i < config.layers[i] ; i++)
+      neuronios.push(new Neuronio(...config.weights[layerIndex][i]));
+
+  return neuronios;
+}
+
 function generateMLP(config) {
   const mlp = [];
-  let i, j;
-  for ( i = 0; i < config.layers.length - 1 ; i++ ) {
-    mlp.push([]);
-    for ( j = 0; j < config.layers[i] ; j++) {
-      mlp[i].push(new Neuronio(...config.weight[j]));
-    }
-  }
+
+  for (var i = 0, l = config.layers.length - 1; i < l; i++ )
+    mlp.push({
+      'isOcult': i > 0 && i < l,
+      'neuronios': getNeuronios(config.layers[i], i, config)
+    });
+
   return mlp;
 }
 
@@ -42,7 +51,6 @@ class MLP {
   }
 
 //inputs [{x1:0.4, x2:0.3}, {x1:0.4, x2:0.3}]
-
 }
 
 module.exports = MLP;
